dfbaadcbb287ab999d30827477aac8b299ae0ead
karenhlai <laikarenh@gmail.com>
2020-03-05 17:35:28 -0800

    Complete Basic Express Server Setup

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.get("/", (req, res) => res.send("Hello Woooorld!"));
+
+app.use(bodyParser.json());
 
 app.listen(5000, () => console.log("Server is running on port 5000"));
