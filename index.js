13d73f457f16850eccc929821564730801633625
karenhlai <laikarenh@gmail.com>
2020-03-05 17:55:25 -0800

    Setup Mongo, Mongoose

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+const db = require('./config/keys').mongoURI;
+
 const bodyParser = require("body-parser");
+
+mongoose
+  .connect(db)
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello Woooorld!"));
 
