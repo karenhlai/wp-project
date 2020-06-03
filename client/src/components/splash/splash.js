3c8e1490478ea2ed724652c9a1403c5468cf6fbc
karenhlai <laikarenh@gmail.com>
2020-06-02 22:54:45 -0700

    Resize splash

--- client/src/components/splash/splash.js
+++ client/src/components/splash/splash.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Homepage from '../../images/homepage.jpg';
 
 const Splash = () => {
   return (
     <div className="splash-container">
+      <img src={Homepage} />
       <Link to="/eyeglasses">Shop Eyeglasses</Link>
     </div>
   );
