80563df3049c7fdd9b4b2bea18d60f969e217ebc
karenhlai <laikarenh@gmail.com>
2020-06-03 22:19:47 -0700

    Update homepage

--- client/src/components/splash/splash.js
+++ client/src/components/splash/splash.js
@@ -4,11 +4,35 @@ import Homepage from '../../images/homepage.jpg';
 
 const Splash = () => {
   return (
-    <div className="splash-container">
-      <img src={Homepage} />
-      <div className="home-nav">
+    <div className="homepage-container">
+      <div className="splash-container">
+        <img src={Homepage} />
         <Link to="/eyeglasses">SHOP EYEGLASSES</Link>
       </div>
+
+      <p>
+        Hello, I used this project to practice code, 
+        <br />
+        It uses tools like React and Node.
+        <br />
+        <br />
+        In addition, I've added GraphQL in between,
+        <br />
+        the server and client layers so data retrieval would be pristine.
+        <br />
+        <br />
+        I've also used Apollo Client to manage state, 
+        <br />
+        so the cached data may quickly update the page.
+        <br />
+        <br />
+        Lastly, this app was made to replicate the features of a cool site, 
+        <br />
+        So, thank Warby Parker for their bright insights. 
+        <br />
+        <br />
+        Enjoy!
+      </p>
     </div>
   );
 }
